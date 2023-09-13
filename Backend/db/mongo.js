36f85d45ee18d1a93525a0be0bd3014f598e9a3a
Rastabann <alexis.godin1207@gmail.com>
2023-09-13 16:16:00 +0200

    MongoDB

--- Backend/db/mongo.js
+++ Backend/db/mongo.js
@@ -1,30 +1,14 @@
 const mongoose = require("mongoose");
-const PASSWORD = "SfK1drGq7fQ5bms1";
-const USER = "godinv5";
-const DB_URL = `mongodb+srv://${USER}:${PASSWORD}@cluster0.hilfbkh.mongodb.net/?retryWrites=true&w=majority`;
+const mongoUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.hilfbkh.mongodb.net/?retryWrites=true&w=majority`;
 
 async function connect() {
   try {
-    await mongoose.connect(DB_URL);
-    console.log("Connected to DB");
+    await mongoose.connect(mongoUrl);
+    console.log("Connected to mongodb");
   } catch (e) {
+    console.error("Error connecting to mongodb");
     console.error(e);
   }
 }
+
 connect();
-
-const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-});
-
-const User = mongoose.model("User", UserSchema);
-
-// const alexis = new User({
-//   email: "godinv5@gmail.com",
-//   password: "alexis",
-// });
-
-// alexis.save().then(() => console.log("alexis saved"));
-
-module.exports = { User };
