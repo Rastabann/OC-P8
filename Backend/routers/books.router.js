8b13c1e80ed84c9e4d24cd4906bca45c07940f4f
Rastabann <alexis.godin1207@gmail.com>
2023-09-15 23:49:34 +0200

    Controllers/routers + Readme

--- Backend/routers/books.router.js
+++ Backend/routers/books.router.js
@@ -1,8 +1,18 @@
+// routers/bookRouter.js
+
 const express = require("express");
-const { Book } = require("../models/Book");
-const { checkToken } = require("../middleware/checkToken");
 const multer = require("multer");
 const { storage } = require("../middleware/storage");
+const { checkToken } = require("../middleware/checkToken");
+const {
+  postRating,
+  getBooks,
+  getBooksWithBestRating,
+  getBook,
+  deleteBook,
+  postBooks,
+  putBook,
+} = require("../controllers/bookController");
 
 const bookRouter = express.Router();
 
@@ -10,146 +20,13 @@ bookRouter.post("/:id/rating", checkToken, postRating);
 bookRouter.get("/", getBooks);
 bookRouter.get("/bestrating", getBooksWithBestRating);
 bookRouter.get("/:id", getBook);
-
 bookRouter.delete("/:id", checkToken, deleteBook);
-
 bookRouter.post(
   "/",
   checkToken,
   multer({ storage: storage }).single("image"),
   postBooks
 );
-
 bookRouter.put("/:id", checkToken, putBook);
+
 module.exports = { bookRouter };
-
-async function postRating(req, res) {
-  const id = req.params.id;
-  const book = await Book.findById(id);
-  const userId = req.body.userIdFromToken;
-  try {
-    const ratings = book.ratings;
-    if (ratings.some((obj) => obj.userId === userId)) {
-      return res.status(400).send("You have already rated this book");
-    }
-    const newRating = {
-      userId: userId,
-      grade: req.body.rating,
-    };
-    ratings.push(newRating);
-
-    const sum = ratings.reduce((total, curr) => (total += curr.grade), 0);
-    const numberOfRaters = ratings.length;
-    const averageRating = sum / numberOfRaters;
-    book.ratings = ratings;
-    book.averageRating = averageRating;
-
-    book.save();
-    res.send(book);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function getBooksWithBestRating(req, res) {
-  const books = await Book.find().sort({ averageRating: -1 }).limit(3);
-  res.send(books);
-}
-
-async function putBook(req, res) {
-  const id = req.params.id;
-  const book = await Book.findById(id);
-  if (!book) return res.status(404).send("Book not found");
-  const userId = book.userId;
-  if (userId !== req.body.userIdFromToken)
-    return res.status(401).send("You can only update your own books");
-  const result = await Book.findOneAndUpdate(
-    { _id: id },
-    {
-      title: req.body.title,
-      author: req.body.author,
-      year: req.body.year,
-      genre: req.body.genre,
-    }
-  );
-  res.send(result);
-}
-
-async function deleteBook(req, res) {
-  const id = req.params.id;
-  const book = await Book.findById(id);
-  if (!book) return res.status(404).send("Book not found");
-  const userIdOnBook = book.userId;
-  if (userIdOnBook !== req.body.userIdFromToken)
-    return res.status(401).send("You can only delete your own books");
-  const result = await Book.findByIdAndDelete(id);
-  res.send(result);
-}
-
-async function getBook(req, res) {
-  const id = req.params.id;
-  const book = await Book.findById(id);
-  book.imageUrl = generateImageUrl(book.imageUrl);
-  res.send(book);
-}
-
-const sharp = require("sharp");
-const path = require("path");
-
-async function postBooks(req, res) {
-  const bookStringified = req.body.book;
-  const book = JSON.parse(bookStringified);
-  const file = req.file;
-
-  try {
-    const initialRating = book.ratings.find(
-      (rating) => rating.userId === book.userId
-    ).grade;
-
-    // Utilisation de sharp pour redimensionner l'image et la convertir en WebP
-    const outputFilename = Date.now() + "-resized.webp";
-    const outputPath = path.join("uploads", outputFilename);
-
-    await sharp(file.path)
-      .resize(200, 200) // Redimensionner à 200px x 200px
-      .webp() // Convertir en WebP
-      .toFile(outputPath);
-
-    const newBook = new Book({
-      userId: book.userId,
-      title: book.title,
-      author: book.author,
-      imageUrl: outputFilename, // Lien avec WebP
-      year: book.year,
-      genre: book.genre,
-      ratings: [
-        {
-          userId: book.userId,
-          grade: initialRating,
-        },
-      ],
-      averageRating: initialRating, // Réglage de la note moyenne initiale pour être la note initiale
-    });
-    await newBook.save();
-    res.send(newBook);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ message: "Error posting the book." });
-  }
-}
-
-async function getBooks(req, res) {
-  const allBooks = await Book.find();
-  allBooks.forEach((book) => {
-    const imageUrl = generateImageUrl(book.imageUrl);
-    book.imageUrl = imageUrl;
-  });
-  res.send(allBooks);
-}
-
-function generateImageUrl(localUrl) {
-  const hostUrl = process.env.HOST_URL;
-  const port = process.env.PORT;
-  const absoluteUrl = hostUrl + ":" + port + "/" + localUrl;
-  return absoluteUrl;
-}
--- README.md
+++ README.md
@@ -1,10 +1,64 @@
 # Mon vieux Grimoire
 
+## Installation
 
-## Comment lancer le projet ? 
+Pour lancer le projet, vous devrez d'abord installer Node.js et MongoDB sur votre machine.
 
-### Avec npm
+Ensuite, installez les dépendances du projet avec npm :
 
-Faites la commande `npm install` pour installer les dépendances puis `npm start` pour lancer le projet. 
+npm install
 
-Le projet a été testé sur node 19. 
+## Configuration
+
+Avant de lancer le projet, vous devrez créer un fichier .env à la racine du projet pour configurer vos variables d'environnement. Vous y définirez le port, l'URL du serveur, le secret JWT, votre nom d'utilisateur et votre mot de passe MongoDB. Voici un exemple de contenu pour le fichier .env :
+
+PORT = 4000
+MONGO_USER = your-user
+MONGO_PASSWORD = your-password
+JWT_SECRET = your-secret
+HOST_URL = http://localhost
+
+Remplacez JWT_SECRET avec la clé secrète de votre choix pour le JWT.
+
+## Lancement du projet
+
+Une fois que vous avez tout configuré, vous pouvez lancer le front-end du projet en utilisant la commande :
+
+npm start
+
+Ensuite, ouvrez un autre terminal et lancez le back-end avec les commandes suivantes :
+
+cd Backend
+npm run dev
+
+## En utilisation
+
+Voici une description de chaque route que vous pouvez utiliser :
+
+POST /books: Ajout d'un livre. Nécessite que le corps de la requête soit sous la forme suivante :
+
+{
+"userId": "<utilisateur-id>",
+"title": "<titre-du-livre>",
+"author": "<nom-de-l'auteur>",
+"image": "<image>",
+"year": "<année-de-publication>",
+"genre": "<genre-du-livre>",
+"ratings": [{
+"userId": "<utilisateur-id>",
+"grade": <note>
+}]
+}
+
+GET /books: Récupère tous les livres.
+POST /books/:id/rating: Ajoute une note à un livre. Nécessite que le corps de la requête soit sous la forme suivante :
+
+{
+"userId": "<utilisateur-id>",
+"rating": <note>
+}
+
+PUT /books/:id: Met à jour un livre. Le corps de la requête doit être dans le même format que pour l'ajout d'un livre.
+DELETE /books/:id: Supprime un livre.
+
+Pour plus d'informations sur l'utilisation de chaque route, veuillez vous référer au code.
